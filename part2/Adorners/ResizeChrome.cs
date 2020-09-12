ea4e4e81fdc2bdaee17e60057ef8ce435537ca32
opensanyue <opensanyue@163.com>
2020-09-12 15:43:53 +0800

    添加项目文件。

---
+++ part2/Adorners/ResizeChrome.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace part2
+{
+    public class ResizeChrome : Control
+    {
+        static ResizeChrome()
+        {
+            FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(ResizeChrome), new FrameworkPropertyMetadata(typeof(ResizeChrome)));
+        }
+    }
+}
---
+++ part2/DesignerCanvas.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Xml;
+
+namespace part2
+{
+    //扩展画布功能
+    public class DesignerCanvas : Canvas
+    {
+        private Point? dragStartPoint = null;
+
+        //添加选中功能
+        public IEnumerable<DesignerItem> SelectedItems
+        {
+            get
+            {
+                var selectedItems = from item in this.Children.OfType<DesignerItem>()
+                                    where item.IsSelected == true
+                                    select item;
+
+                return selectedItems;
+            }
+        }
+
+        //添加不选中功能
+        public void DeselectAll()
+        {
+            foreach (DesignerItem item in this.SelectedItems)
+            {
+                item.IsSelected = false;
+            }
+        }
+
+        //鼠标按下时，点在画布上，取消所有选择
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(e);
+            if (e.Source == this)
+            {
+                this.dragStartPoint = new Point?(e.GetPosition(this));
+                this.DeselectAll();
+                e.Handled = true;
+            }
+        }
+
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+
+            if (e.LeftButton != MouseButtonState.Pressed)
+            {
+                this.dragStartPoint = null;
+            }
+
+            if (this.dragStartPoint.HasValue)
+            {
+                AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(this);
+                if (adornerLayer != null)
+                {
+                    RubberbandAdorner adorner = new RubberbandAdorner(this, this.dragStartPoint);
+                    if (adorner != null)
+                    {
+                        adornerLayer.Add(adorner);
+                    }
+                }
+
+                e.Handled = true;
+            }
+        }
+
+        protected override void OnDrop(DragEventArgs e)
+        {
+            base.OnDrop(e);
+            string xamlString = e.Data.GetData("DESIGNER_ITEM") as string;
+            if (!String.IsNullOrEmpty(xamlString))
+            {
+                DesignerItem newItem = null;
+                FrameworkElement content = XamlReader.Load(XmlReader.Create(new StringReader(xamlString))) as FrameworkElement;
+
+                if (content != null)
+                {
+                    newItem = new DesignerItem();
+                    newItem.Content = content;
+
+                    Point position = e.GetPosition(this);
+                    if (content.MinHeight != 0 && content.MinWidth != 0)
+                    {
+                        newItem.Width = content.MinWidth * 2; ;
+                        newItem.Height = content.MinHeight * 2;
+                    }
+                    else
+                    {
+                        newItem.Width = 65;
+                        newItem.Height = 65;
+                    }
+                    DesignerCanvas.SetLeft(newItem, Math.Max(0, position.X - newItem.Width / 2));
+                    DesignerCanvas.SetTop(newItem, Math.Max(0, position.Y - newItem.Height / 2));
+                    this.Children.Add(newItem);
+
+                    this.DeselectAll();
+                    newItem.IsSelected = true;
+                }
+
+                e.Handled = true;
+            }
+        }
+
+
+        //WPF控件提供一个MeassureOverride允许控件计算希望的大小，再返回WPF框架来进行
+        //布局。我们可以在DesignerCanvas中重载这个方法来解决上面所说的问题，重载方法如下：
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            Size size = new Size();
+            foreach (UIElement element in Children)
+            {
+                double left = Canvas.GetLeft(element);
+                double top = Canvas.GetTop(element);
+                left = double.IsNaN(left) ? 0 : left;
+                top = double.IsNaN(top) ? 0 : top;
+                //按照限定的尺寸测量一下自己，拿镜子找着自己
+                element.Measure(constraint);
+
+                Size desiredSize = element.DesiredSize;
+                if (!double.IsNaN(desiredSize.Width) && !double.IsNaN(desiredSize.Height))
+                {
+                    size.Width = Math.Max(size.Width, left + desiredSize.Width);
+                    size.Height = Math.Max(size.Height, top + desiredSize.Height);
+                }
+            }
+
+            // add some extra margin
+            size.Width += 10;
+            size.Height += 10;
+            //申报，我需要这个尺寸
+            return size;
+        }
+    }
+}
